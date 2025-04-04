f5a34ddad430e5a32e10b6064a13e46599575382
ChaserHati <catalina.zuniga.y@gmail.com>
2025-04-04 15:09:13 -0300

    esqueleto ficha + dashboard + sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import {MatButtonModule} from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, MatSidenavModule, MatButtonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FichaComponent } from './ficha/ficha.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: 'dashboard', component: DashboardComponent},
+    {path: 'ficha', component: FichaComponent},
+];
