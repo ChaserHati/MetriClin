8709c1cf6aa8b91f2628c6a538a05b3193d15257
ChaserHati <catalina.zuniga.y@gmail.com>
2025-04-08 19:51:28 -0400

    cambiar algunos componentes de bootstrap a angular material

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [MatButtonModule,],
+  imports: [CommonModule,MatButtonModule, MatCardModule, MatInputModule, MatFormFieldModule, MatIconModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+
+  cards = [
+    { title: 'Card 1', content: 'This is card 1' },
+    { title: 'Card 2', content: 'This is card 2' },
+    { title: 'Card 3', content: 'This is card 3' },
+    { title: 'Card 4', content: 'This is card 4' },
+    { title: 'Card 5', content: 'This is card 5' },
+    { title: 'Card 6', content: 'This is card 6' },
+    { title: 'Card 7', content: 'This is card 7' },
+    { title: 'Card 8', content: 'This is card 8' },
+  ];
 
 }
--- src/app/ficha/ficha.component.ts
+++ src/app/ficha/ficha.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 @Component({
   selector: 'app-ficha',
-  imports: [],
+  imports: [CommonModule, MatButtonModule, MatInputModule, MatFormFieldModule, MatDatepickerModule],
   templateUrl: './ficha.component.html',
+  providers: [provideNativeDateAdapter()],
   styleUrl: './ficha.component.css'
 })
 export class FichaComponent {
