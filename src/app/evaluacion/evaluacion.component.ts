a4e07efdba9bd084a4953b8596fd1b8da8bb4979
ChaserHati <catalina.zuniga.y@gmail.com>
2025-04-13 02:23:28 -0400

    modales en evaluacion + variables ngModel

--- src/app/evaluacion/evaluacion.component.ts
+++ src/app/evaluacion/evaluacion.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
@@ -11,6 +12,7 @@ import {provideNativeDateAdapter} from '@angular/material/core';
   selector: 'app-evaluacion',
   imports: [
     CommonModule,
+    FormsModule,
     MatInputModule,
     MatButtonModule,
     MatFormFieldModule,
@@ -21,5 +23,27 @@ import {provideNativeDateAdapter} from '@angular/material/core';
   styleUrl: './evaluacion.component.css'
 })
 export class EvaluacionComponent {
+
+peso:number=0;
+talla:number=0;
+
+plBicipital:number=0;
+plTricipital:number=0;
+plSubescapular:number=0;
+plCrestaIliaca:number=0;
+plEspinaIliaca:number=0;
+plAbdominal:number=0;
+plMuslo:number=0;
+plPantorrilla:number=0;
+
+perBrazo:number=0;
+perCintura:number=0;
+perCadera:number=0;
+perMuslo:number=0;
+perPantorrilla:number=0;
+
+diaHumero:number=0;
+diaMuneca:number=0;
+diaFemur:number=0;
 
 }
